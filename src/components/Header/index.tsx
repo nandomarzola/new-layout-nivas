a3a9423c808dc89c4a540d905e4a290bf16f060a
itMarzola <itmarzola@rd.com.br>
2024-03-27 14:02:24 -0300

    saving imagens in home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,35 +14,24 @@ export default function Header(){
     const navigate = useNavigate();
 
     return (
-        <Container style={{marginLeft:'16px', marginRight:'16px'}}>
+        <Container>
             <Title>PARCEIROS</Title>
-            <div style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px', marginTop:'15px'}}>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px',}}>
                 <img 
                     src="images/logo-transparent.png"  
                     alt="Omega"
-                    style={{height:'8vh'}}
+                    style={{height:'14vh'}}
                 />
-                  <img 
-                    src="https://3ff5263b8f.cbaul-cdnwnd.com/ca391f83a3a3faaf706896ebafa56111/200000078-ac999ac99c/logo%20omega%20PNG.png?ph=3ff5263b8f"  
-                    alt="Omega"
-                    style={{height:'8vh', marginTop:'9px'}}
-                />
-            </div>
-            <HorizontalLine />
-            <div style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px', marginTop:'15px'}}>
+            </div>  
+            <HorizontalLine />       
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px', }}>
                 <img 
-                    src="images/logo-transparent.png"  
-                    alt="Omega"
-                    style={{height:'8vh'}}
-                />
-                  <img 
                     src="https://3ff5263b8f.cbaul-cdnwnd.com/ca391f83a3a3faaf706896ebafa56111/200000078-ac999ac99c/logo%20omega%20PNG.png?ph=3ff5263b8f"  
                     alt="Omega"
-                    style={{height:'8vh'}}
-                />
-            </div>
+                />    
+            </div>  
             <HorizontalLine />
-            <Button onClick={() => navigate('/menu')} style={{marginTop:'5vh'}}>
+            <Button onClick={() => navigate('/menu')}>
                 <HiArrowCircleDown size={32} />
                 <p>Ver cárdapio completo</p>
             </Button>
