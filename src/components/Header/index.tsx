2a0e6cc16c36a7798309edefc98b1e6e9b168289
itMarzola <itmarzola@rd.com.br>
2024-04-22 12:29:08 -0300

    teste 2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,87 @@
+import { useNavigate } from 'react-router';
 
+import {
+    Container,
+    Title,
+    HorizontalLine,
+    Button
+} from './styles'
 
+import { HiArrowCircleDown } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 export default function Header(){
 
+    const navigate = useNavigate();
+
     return (
-        <div style={{color:'#fff'}}> 
-           ola mundo
-        </div>
+        <Container>
+            <Title>PARCEIROS</Title>
+            <motion.div
+                initial={{ x: -400, opacity: 0 }} 
+                animate={{ x: 0, opacity: 1 }} 
+                transition={{ duration: 1.5 }}
+                style={{ width: '12v'}} 
+            >
+                <a href="https://www.instagram.com/nandomarzoladev/" style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px',}}>
+                    <img 
+                        src="images/logo-transparent.png"  
+                        alt="Nando Marzola"
+                        style={{height:'12vh'}}
+                    />
+                </a>  
+                <HorizontalLine />       
+            </motion.div>
+            
+            <motion.div
+                initial={{ x: 500, opacity: 0 }} 
+                animate={{ x: 0, opacity: 1 }} 
+                transition={{ duration: 1.5 }}
+                style={{ width: '12v'}} 
+            >
+                <a href="https://www.instagram.com/omegatechservice/" style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px', }}>
+                    <img 
+                        src="images/omega.png"  
+                        alt="Omega"
+                        style={{width:'36vh'}}
+                    />    
+                </a>  
+                <HorizontalLine />
+            </motion.div>
+            <motion.div
+                initial={{ x: -400, opacity: 0 }} 
+                animate={{ x: 0, opacity: 1 }} 
+                transition={{ duration: 1.5 }}
+                style={{ width: '12v'}} 
+            >      
+                <a href="https://www.instagram.com/planetpetmarilia/" style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px',}}>
+                    <img 
+                        src="images/logo-planet.png"  
+                        alt="Planet Pet"
+                        style={{height:'6vh',  marginTop:'10px', marginBottom:'10px'}}
+                    />
+                </a>  
+                <HorizontalLine />
+            </motion.div>
+            <motion.div
+                initial={{ x: 500, opacity: 0 }} 
+                animate={{ x: 0, opacity: 1 }} 
+                transition={{ duration: 1.5 }}
+                style={{ width: '12v'}} 
+            >   
+                <a href="https://www.instagram.com/zoiomotoszm/" style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'5px',}}>
+                    <img 
+                        src="images/zoio.png"  
+                        alt="Planet Pet"
+                        style={{height:'13vh',  marginTop:'10px', marginBottom:'10px'}}
+                    />
+                </a>  
+                <HorizontalLine />
+            </motion.div>
+            <Button onClick={() => navigate('/menu')} style={{marginTop:'4vh'}}>
+                <HiArrowCircleDown size={32} />
+                <p>Ver cárdapio completo</p>
+            </Button>
+        </Container>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,46 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+   display:flex;
+   justify-content:center;
+   flex-direction:column;
+   padding: 1rem;
+   color:#FFF;
+`;
+
+export const ImgLogo = styled.img`
+   width:12rem;
+   margin:0 auto;
+`;
+
+export const HorizontalLine = styled.hr`
+   border: 0;
+   height: 1px; /* Altura da linha */
+   background: linear-gradient(to right, #888, #F29F05, #888); /* Gradiente de cores */
+   margin: 10px 0; /* Espaçamento acima e abaixo da linha */
+`;
+
+export const Title = styled.h1`
+   font-size:18px;
+   padding:5px;
+   width:240px;
+   border-radius:8px;
+   color:#F29F05;
+`
+
+export const Text = styled.p`
+   font-family: "Roboto", sans-serif;
+   margin-top:10px;
+   font-weight:bold;
+   margin-bottom:12px;
+`
+
+export const TextSmall = styled.p`
+   font-size:10px;
+   font-family: "Roboto", sans-serif;
+   max-width:250px;
+
+`
 
 export const Price = styled.p`
    font-size:12px;
@@ -8,3 +49,19 @@ export const Price = styled.p`
    font-family: "Roboto", sans-serif;
 `
 
+export const Button = styled.button`
+   background: #F29F05;
+   font-family: "Roboto", sans-serif;
+   border:0;
+   height:45px;
+   border-radius:8px;
+   display:flex;
+   justify-content:center;
+   align-items:center;
+   text-transform:uppercase;
+   font-weight:bold;
+   margin-top:12px;
+   font-size:14px;
+   gap:10px;
+   cursor:pointer;
+`  
