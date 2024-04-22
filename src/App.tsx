5781cd43183c4c53b05e255b6694e4f2e019318d
itMarzola <itmarzola@rd.com.br>
2024-04-22 12:13:41 -0300

    new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom"
-import Logo from "./components/Logo"
 
 function App() {
 
   return (
     <>
       <Outlet />
-      {/* <Footer /> */}
     </>
   )
 }
